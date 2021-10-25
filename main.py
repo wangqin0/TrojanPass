acbbf0c71027c9b40dbe5cc12720f17f32fcdbe9
Wang Qin <wangqin0@outlook.com>
2021-10-25 11:40:54 -0700

    Update logging and README.md

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from datetime import date
 import get_pass
 import send_email
 
+logging.getLogger().setLevel(logging.INFO)
+logging.basicConfig(format='%(asctime)s %(message)s')
 
 DIR_NAME = 'saved_passes'
 output_image = 'trojan-pass-' + str(date.today()) + '.png'
@@ -18,8 +20,11 @@ if 'TROJAN_PASS_NETID' not in os.environ:
     logging.error("It is possible that you're using crontab and this requires further setup")
 
 if not Path(DIR_NAME).exists():
-    Path("DIR_NAME").mkdir(parents=True, exist_ok=False)
+    Path(DIR_NAME).mkdir(parents=True, exist_ok=False)
     logging.info('directory "' + DIR_NAME + '" not exists and got created')
+
+os.chdir(DIR_NAME)
+
 
 next_test_remainder = get_pass.get_pass_and_remainder(output_image)
 
@@ -28,7 +33,7 @@ logging.debug(output_image + ' is saved and next_test_remainder is "' + next_tes
 send_email.send_from_gmail(os.getenv('TROJAN_PASS_NETID') + "@usc.edu",
                            "Your Daily Trojan Pass",
                            next_test_remainder + "\n\nHave a good day! XD",
-                           os.path.join(DIR_NAME, output_image),
+                           output_image,
                            os.getenv('TROJAN_PASS_GMAIL_ACCOUNT'),
                            os.getenv('TROJAN_PASS_GMAIL_PASSWORD'))
 
