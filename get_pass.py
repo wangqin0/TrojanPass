532ce1be0ea5d837b087a4710d2061e5c6e8ba09
Tianli Tong <tongtianli@126.com>
2021-10-25 13:44:00 -0700

    Multiuser support

--- get_pass.py
+++ get_pass.py
@@ -10,7 +10,7 @@ def get_pass_and_remainder(net_id, net_pw, str_today) -> str:
     # Requires Selenium WebDriver 3.13 or newer
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     # chrome_options.add_argument('--disable-dev-shm-usage')
