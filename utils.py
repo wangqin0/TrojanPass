073cde06c5df5a139280e89b2edcb0d91d4d4e1e
Tianli Tong <tongtianli@126.com>
2021-11-01 15:45:11 -0700

    add usc email parser

--- utils.py
+++ utils.py
@@ -10,3 +10,7 @@ def str_image(net_id: str):
     save_path = os.environ.get('SAVE_PATH') or "saved_passes"
     image_name = f'{net_id}-' + str_today() + '.png'
     return f"{save_path}/{image_name}"
+
+
+def usc_email_address_for(net_id: str):
+    return f"{net_id}@usc.edu"
